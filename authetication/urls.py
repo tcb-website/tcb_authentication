ec180fd258dde20af2bc2c63955169b9bc7c207a
Anjali2201 <anjalikushwaha031@gmail.com>
2022-05-24 21:20:23 +0530

    files uploaded

---
+++ authetication/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home,name="home"),
+    path('signup',views.signup,name="signup"),
+    path('signin',views.signin,name="signin"),
+    path('signout',views.signout,name="signout"),
+    path('activate/<uidb64>/<token>',views.activate ,name="activate")
+]
